4ec22eb5747cde1ad1a7edd82c8071b4edd3a59b
Benny Li <dev@benny-li.de>
2019-02-14 11:29:50 +0100

    Better viewing with theme for rofi network selection

--- rofi/scripts/network-select.sh
+++ rofi/scripts/network-select.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env sh
 
-selection=$(ls -p /etc/netctl | grep -v / | rofi -dmenu -mesg "Select network profile")
+selection=$(ls -p /etc/netctl | grep -v / | rofi -dmenu -p "Select network profile")
 
 sudo netctl start $selection
