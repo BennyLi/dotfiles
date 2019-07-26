8097fd81d20f9a25ce4783d32eb6ab3de880f1ea
Benny Li <dev@benny-li.de>
2019-07-26 12:08:29 +0200

    Dunst config and test file

---
+++ dunst/test-notifications.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env sh
+
+notify-send --urgency=low --app-name="Low App" "Low Notification"
+notify-send --urgency=normal --app-name="Normal App" "Normal Notification"
+notify-send --urgency=critical --app-name="Critical App" "Critical Notification"
