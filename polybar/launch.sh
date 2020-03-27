9cd8be81e793ad3951cb8c64ff5a030fba04a54b
Benny Li <dev@benny-li.de>
2020-03-27 13:47:44 +0100

    🔧 Added polybar config + autostart to i3wm

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,5 +6,11 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch 
-polybar example &
+# Launch polybar on all monitors connected
+if type "xrandr"; then
+  for monitor in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$monitor polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
